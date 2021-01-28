ac82d22c883af8d198711f25dfc714901cc1b74c
AmbyMbayi <ambymbayi54@gmail.com>
2021-01-28 12:03:05 +0300

    layout file(using @extends, @section and @yield)

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>pizza hub</title>
+</head>
+
+<body>
+    <nav>
+        <h2>Pizza Hub</h2>
+    </nav>
+    <div>
+        @yield('content')
+    </div>
+    <footer>copyright 2021 PizzaHouse</footer>
+</body>
+
+</html>
--- resources/views/pizza.blade.php
+++ resources/views/pizza.blade.php
@@ -1,27 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
+@section('content')
 
 
+<h2>Piza list</h2>
+@for ($i = 0; $i < count($pizza); $i++) <p>{{ $pizza[$i]['type'] }}</p>
+    @endfor
 
-    @for ($i = 0; $i < count($pizza); $i++) <p>{{ $pizza[$i]['type'] }}</p>
-        @endfor
+    @foreach ($pizza as $piz)
+    <div>
+        {{ $loop->index }} {{ $piz['type'] }} - {{ $piz['base'] }}
+    </div>
+    @endforeach
 
-        @foreach ($pizza as $piz)
-        <div>
-            {{ $loop->index }} {{ $piz['type'] }} - {{ $piz['base'] }}
-        </div>
-        @endforeach
-
-
-</body>
-
-</html>
+    @endsection
