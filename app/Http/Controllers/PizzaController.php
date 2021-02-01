cf61db6400a27a1fec45f099810bacc4dc4f691e
AmbyMbayi <ambymbayi54@gmail.com>
2021-02-01 12:40:57 +0300

    logging data using post and redirecting

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -26,4 +26,14 @@ class PizzaController extends Controller
     public function create(){
         return view('pizzas.create');
     }
+    public function store(){
+
+        error_log(request('name'));
+        error_log(request('type'));
+        error_log(request('price'));
+        error_log(request('base'));
+
+        return redirect('/pizzas');
+
+    }
 }
--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -6,6 +6,8 @@
 <div class="wrapper create-pizza">
     <h1>Create a new pizza</h1>
     <form action="/pizzas" method="POST">
+        @csrf
+        
         <label for="name">Your name</label>
         <input type="text" id="name" name="name">
         <label for="type">Choose Pizza type:</label>
