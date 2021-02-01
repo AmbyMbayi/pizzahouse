febbff2d4ca4f8c2a38e4b7a632b8463cef25439
AmbyMbayi <ambymbayi54@gmail.com>
2021-02-01 12:25:12 +0300

    create form

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -3,11 +3,29 @@
 @section('content')
 
 
-<h2 class="header">Piza list</h2>
-
-   <div>
-       add new pizza
-   </div>
+<div class="wrapper create-pizza">
+    <h1>Create a new pizza</h1>
+    <form action="/pizzas" method="POST">
+        <label for="name">Your name</label>
+        <input type="text" id="name" name="name">
+        <label for="type">Choose Pizza type:</label>
+        <select name="type" id="type">
+            <option value="magarita">Magarita</option>
+            <option value="hawaiian">Hawaiian</option>
+            <option value="volcano">Volcano</option>
+        </select>
+        <label for="type">Choose base type:</label>
+        <select name="base" id="base">
+            <option value="cheesy crust">Cheesy Crust</option>
+            <option value="garlic crust">Garlic Crust</option>
+            <option value="volcano">Thin & Crispy</option>
+            <option value="thick">Thick</option>
+        </select>
+        <label for="price">Price</label>
+        <input type="number" name="price">
+        <input type="submit" value="order Pizza">
+    </form>
+</div>
   
    
 
--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -3,12 +3,14 @@
 @section('content')
 
 
-<h2 class="header">Piza list</h2>
+<h2 class="header">Piza list</h2> <a href="/pizzas/create">Create Pizza</a>
 
     @foreach ($pizzas as $pizza)
     <div>
        {{$pizza-> name}} - {{$pizza-> type}} - {{$pizza ->base}}
+     
     </div>
+   
     @endforeach
 
     @endsection
