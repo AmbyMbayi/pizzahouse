2e752d50104a13fb249bdff7584a51bcb5741f9b
AmbyMbayi <ambymbayi54@gmail.com>
2021-02-01 11:51:01 +0300

    getting single record(show view)

--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -2,9 +2,12 @@
 
 @section('content')
 
-
-<div>
-    pizza list ={{ $id }}
+<div class="wrapper pizza-details">
+    <h1>Order for {{$pizza->name}}</h1>
+    <p class="type">The type is -{{$pizza ->type}}</p>
+    <p class="type">The price is -{{$pizza ->price}}</p>
+    <p class="type">The base is -{{$pizza ->base}}</p>
 </div>
+<a href="/pizza" class="back">Back to all Pizza</a>
 
 @endsection
